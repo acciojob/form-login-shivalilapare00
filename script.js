4c5db0e9d3e0d754ddef3d197e7ee828f120bfc7
shivalilapare00 <90516920+shivalilapare00@users.noreply.github.com>
2025-11-29 09:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function getFormvalue() {
     //Write your code here
+	
+    event.preventDefault(); // Stop form from reloading the page
 
+    // Get the form element
+    const form = document.getElementById("form1");
+
+    // Retrieve values & trim extra spaces
+    const fname = form.fname.value.trim();
+    const lname = form.lname.value.trim();
+
+    // Create full name
+    const fullName = fname + " " + lname;
+
+    // Show alert
+    alert(fullName);
 }
+
+
+
